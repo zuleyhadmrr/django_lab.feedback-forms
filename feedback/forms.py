251c1c9b2515b782fce17a5bf9bb0cc5486b6741
zuleyhamdrr <zuleyha.d0542@gmail.com>
2026-08-07 22:38:46 +0300

    Add custom validators to FeedbackForm

--- feedback/forms.py
+++ feedback/forms.py
@@ -11,3 +11,16 @@ class FeedbackForm (forms.ModelForm):
             'message',
             'rating',
         ]
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        if len(message) < 20:
+            raise forms.ValidationError("Message must be at least 20 characters.")
+        return message
+
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) < 3:
+            raise forms.ValidationError("Name must be at least 3 characters.")
+        return name
